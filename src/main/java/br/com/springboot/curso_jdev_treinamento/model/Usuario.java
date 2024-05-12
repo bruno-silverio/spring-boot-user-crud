6954026cfe181cd6e2239798b5806f0f3dd6d867
Bruno Silverio <bcamilo94@gmail.com>
2024-05-11 21:32:35 -0300

    feat: add the model and database

---
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/model/Usuario.java
@@ -0,0 +1,49 @@
+package br.com.springboot.curso_jdev_treinamento.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+@SequenceGenerator(name = "seq_user", sequenceName = "seq_user", allocationSize = 1, initialValue = 1)
+public class Usuario implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_user")
+	private Long id;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	private String name;
+		
+	private int age;
+
+}
