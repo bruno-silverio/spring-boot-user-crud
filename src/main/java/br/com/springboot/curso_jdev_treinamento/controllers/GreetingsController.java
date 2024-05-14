a6e85b8e41dbe2c5b4ea6b59286d3b506fc2c9f2
Bruno Silverio <bcamilo94@gmail.com>
2024-05-13 23:08:55 -0300

    feat: add save user

--- src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -49,11 +51,19 @@ public class GreetingsController {
 		return "Ola mundo " + name;
 	}
 
-	@GetMapping(value = "listall")
-	@ResponseBody
-	public ResponseEntity<List<Usuario>> listaUsuario() {
-		List<Usuario> usuarios = usuarioRepository.findAll();
+	@GetMapping(value = "listall") // URL
+	@ResponseBody /* Retorna os dados par ao corpo da resposta */
+	public ResponseEntity<List<Usuario>> userList() {
+		List<Usuario> usuarios = usuarioRepository.findAll(); /* executa a consulta no banco de dados */
 
-		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
+		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK); /* Retorna a lista em JSON */
+	}
+	
+	@PostMapping(value = "save") // URL
+	@ResponseBody /* Descricao da resposta */
+	public ResponseEntity<Usuario> save(@RequestBody Usuario usuario) { /* Recebe os dados para salvar */
+		Usuario user = usuarioRepository.save(usuario); 
+		
+		return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
 	}
 }
